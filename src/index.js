8dd8fced98717e38fa37776c92dd8844c4bcb8cf
pas801012 <48752952+pas801012@users.noreply.github.com>
2019-09-14 15:42:14 +0300

    upload
    Add upload dir

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-var http = require("http");
+var express = require("express");
+var app = express();
+var workdir = "public";
+var uploadDir = "upload";
+var uploadLocation = [workdir, uploadDir].join("/"); //public/upload
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const fs = require("fs");
+
+app.use(express.static(workdir));
+
+app.post("/upload", (req, res) => {
+  let fileName = Math.random().toString("36");
+  let fileLocation = [uploadLocation, fileName].join("/"); //`upload/${fileName}`;
+  let fileStream = fs.createWriteStream(fileLocation);
+
+  req.pipe(fileStream);
+  req.on("end", () => {
+    res.end([uploadLocation, fileName].join("/")); // public/upload/{fileName}
+  });
+});
+
+app.get("/upload", (req, res) => {
+  const fileNames = fs.readdirSync(uploadLocation);
+  console.log("filenames", fileNames);
+  res.send(JSON.stringify(fileNames.map()));
+});
+
+console.log(fs.readdirSync(uploadLocation));
+// fs.readdirSync(uploadLocation);
+console.log("this is test ", uploadLocation);
+
+app.listen(8080, function() {
+  console.log("Example app listening on port 8080!");
+});
+
+// var http = require("http");
+
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
